bf13e1d12327be5150e9006f2f200e8d2b5389a0
Tom <tom@eygrr.com>
2017-09-12 10:43:03 +0100

    Fixed topic model cross val

--- src/topicmodel.py
+++ src/topicmodel.py
@@ -48,7 +48,7 @@ def LDA(tf, names, components, file_name,   doc_topic_prior, topic_word_prior,  
 
 def main(data_type, class_labels_fn, class_names_fn, feature_names_fn, max_depth, limit_entities,
          limited_label_fn, vector_names_fn, dt_dev, doc_topic_prior, topic_word_prior, n_topics, file_name, final_csv_name,
-         high_amt, low_amt):
+         high_amt, low_amt, cross_val):
 
     print("importing class all")
     tf = dt.import2dArray("../data/"+data_type+"/bow/frequency/phrases/class-all-"+str(high_amt)+"-"+str(low_amt)+"-all")
@@ -56,44 +56,49 @@ def main(data_type, class_labels_fn, class_names_fn, feature_names_fn, max_depth
     variables_to_execute = list(product(doc_topic_prior, topic_word_prior, n_topics))
     print("executing", len(variables_to_execute), "variations")
     csvs = []
+    csv_fns = []
     file_names = []
     for vt in variables_to_execute:
         doc_topic_prior = vt[0]
         topic_word_prior = vt[1]
         n_topics = vt[2]
         file_names.append(file_name + "DTP" + str(doc_topic_prior) + "TWP" + str(topic_word_prior) + "NT" + str(n_topics))
+    final_csv_fn = "../data/"+data_type+"/rules/tree_csv/"+ file_name + final_csv_name + ".csv"
     for vt in range(len(variables_to_execute)):
         doc_topic_prior = variables_to_execute[vt][0]
         topic_word_prior = variables_to_execute[vt][1]
         n_topics = variables_to_execute[vt][2]
         file_name = file_names[vt]
-
         LDA(tf, names, n_topics, file_name,
             doc_topic_prior, topic_word_prior, data_type)
 
         #NMFFrob(dt.import2dArray("../data/"+data_type+"/bow/ppmi/class-all-100-10-all"),  dt.import1dArray("../data/"+data_type+"/bow/names/100.txt"), 200, file_name)
 
         topic_model_fn = "../data/" + data_type + "/LDA/rep/" + file_name + ".txt"
+        cv_fns = []
+        og_fn = file_name
+        for c in range(cross_val):
+            file_name = og_fn + " " + str(cross_val) + "CV " + str(c)
+            csv_name = "../data/" + data_type + "/rules/tree_csv/" + file_name + ".csv"
+            cv_fns.append(csv_name)
 
-        csv_name = "../data/" + data_type + "/rules/tree_csv/" + file_name + ".csv"
-        csvs.append(csv_name)
+            tree.DecisionTree(topic_model_fn, class_labels_fn, class_names_fn, feature_names_fn, file_name, 10000,
+                              max_depth=max_depth, balance="balanced", criterion="entropy", save_details=True, cv_splits=cross_val,
+                              split_to_use=c,  data_type=data_type, csv_fn=csv_name, rewrite_files=False, development=dt_dev,
+                              limit_entities=limit_entities,
+                              limited_label_fn=limited_label_fn, vector_names_fn=vector_names_fn, clusters_fn=topic_model_fn,
+                              cluster_duplicates=True, save_results_so_far=False)
 
-        tree.DecisionTree(topic_model_fn, class_labels_fn, class_names_fn, feature_names_fn, file_name, 10000,
-                          max_depth=max_depth, balance="balanced", criterion="entropy", save_details=True, cv_splits=1,
-                          split_to_use=0,
-                          data_type=data_type, csv_fn=csv_name, rewrite_files=False, development=dt_dev,
-                          limit_entities=limit_entities,
-                          limited_label_fn=limited_label_fn, vector_names_fn=vector_names_fn, clusters_fn=topic_model_fn,
-                          cluster_duplicates=True, save_results_so_far=False)
-
-        tree.DecisionTree(topic_model_fn, class_labels_fn, class_names_fn, feature_names_fn, file_name + "None", 10000,
-                          max_depth=None, balance="balanced", criterion="entropy", save_details=False,
-                          data_type=data_type, csv_fn=csv_name, rewrite_files=False,
-                          cv_splits=1, split_to_use=0, development=dt_dev, limit_entities=limit_entities,
-                          limited_label_fn=limited_label_fn, vector_names_fn=vector_names_fn, clusters_fn=topic_model_fn,
-                          cluster_duplicates=True, save_results_so_far=False)
-
-    dt.arrangeByScore(np.unique(np.asarray(csvs)), "../data/"+data_type+"/rules/tree_csv/"+file_name + final_csv_name + ".csv")
+            tree.DecisionTree(topic_model_fn, class_labels_fn, class_names_fn, feature_names_fn, file_name + "None", 10000,
+                              max_depth=None, balance="balanced", criterion="entropy", save_details=False,
+                              data_type=data_type, csv_fn=csv_name, rewrite_files=False,cv_splits=cross_val,
+                              split_to_use=c,  development=dt_dev, limit_entities=limit_entities,
+                              limited_label_fn=limited_label_fn, vector_names_fn=vector_names_fn, clusters_fn=topic_model_fn,
+                              cluster_duplicates=True, save_results_so_far=False)
+        dt.averageCSVs(cv_fns)
+        file_name = og_fn + " " + str(cross_val) + "CV " + str(0)
+        csvs.append("../data/" + data_type + "/rules/tree_csv/" + file_name + "AVG.csv")
+    dt.arrangeByScore(np.unique(np.asarray(csvs)), final_csv_fn)
 data_type = "placetypes"
 high_amt = 50
 low_amt = 10
@@ -105,7 +110,7 @@ limit_entities = False
 dt_dev = True
 vector_names_fn = "../data/" + data_type + "/nnet/spaces/entitynames.txt"
 
-
+cross_val = 5
 
 doc_topic_prior = {0.1, 0.01, 0.001}
 topic_word_prior = {0.1, 0.01, 0.001}
@@ -118,4 +123,4 @@ for c in classify:
     limited_label_fn = "../data/" + data_type + "/classify/" + c + "/available_entities.txt"
     if  __name__ =='__main__':main(data_type, class_labels_fn, class_names_fn, feature_names_fn, max_depth, limit_entities,
              limited_label_fn, vector_names_fn, dt_dev, doc_topic_prior, topic_word_prior, n_topics, file_name, final_csv_name,
-                                   high_amt, low_amt)
+                                   high_amt, low_amt, cross_val)
